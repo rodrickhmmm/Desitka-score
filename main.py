ac9df03bf1b607416641d010f216c679fcc87ae6
Rodrick__ <tom.rodrick100@gmail.com>
2025-06-01 13:21:12 +0200

    Dalsi commit, trochu jsem predelal menu do stylu material ui 3 xdd

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from PIL import Image,ImageTk
+from tkinter.colorchooser import askcolor
+import customtkinter
 
 #nastavení grafiky
 root = tk.Tk()
@@ -12,7 +14,7 @@ image = Image.open("logo.png")
 image = image.resize((400+100,120))
 pic = ImageTk.PhotoImage(image)
 
-canvas = tk.Canvas(root, width=canvas_width, height=canvas_height, bg="darkgrey")
+canvas = tk.Canvas(root, width=canvas_width, height=canvas_height, bg="#32373B")
 canvas.pack()
 
 button1 = None
@@ -20,39 +22,56 @@ button2 = None
 button3 = None
 
 def clearnutigui():
-    global button1, button2
-    canvas.delete("all")
+    global button1, button2, button3
+    #canvas.destroy("all")
     if button1:
         button1.destroy()
     if button2:
         button2.destroy()
     if button3:
         button3.destroy()
+        
+def vytvorit_tlacitko(text, command, y):
+    btn = customtkinter.CTkButton(
+        canvas,
+        text=text,
+        fg_color='#677279',
+        font=("Poppins", 50),
+        width=canvas_width / 1.1,
+        height=90,
+        command=command,
+        cursor="hand2",
+        corner_radius=100000,
+        hover_color="#4B5358",
+        border_width=5
+    )
+    btn.place(x=25, y=y + padding)
+    return btn
 
 def hostovani():
     clearnutigui()
+    canvas.create_text(canvas_width/2,300+padding,text="Hostování skóre",fill="white", font=("Poppins", 40, "bold" ))
+    canvas.create_text(canvas_width/2,350+padding,text="Počkej, než se napojí všichni hráči",fill="white", font=("Poppins", 20,))
+    button4 = vytvorit_tlacitko("Zpátky do menu",main_menu,900)
 
 def pripojeni():
     clearnutigui()
 
+padding = 20
+    
 def main_menu():
     global button1, button2, button3
 
-    canvas.create_rectangle(0,0,1080/2,200,fill="grey")
-    canvas.create_text(canvas_width/2,150,text="Score",fill="white", font=("Helvetica", 50, "bold"))
+    canvas.create_rectangle(0,0,canvas_width,220+padding,fill="#545D63")
+    canvas.create_image(canvas_width/2,50+padding,image=pic)
+    canvas.create_text(canvas_width/2,160+padding,text="SCORE",fill="black", font=("Poppins", 70, "bold" ))
 
+    button1 = vytvorit_tlacitko("Hostuj",hostovani,300)
 
-    canvas.create_image(canvas_width/2,55,image=pic)
+    button2 = vytvorit_tlacitko("Připoj",pripojeni,430)
 
-    button1 = tk.Button(canvas, text="Hostuj", bg='grey', fg='white', font=("Helvetica", 30, "bold"), width=20, command=hostovani)
-    button1.place(x=25, y=300)
-
-    button2 = tk.Button(canvas, text="Připoj", bg='grey', fg='white', font=("Helvetica", 30, "bold"), width=20, command=pripojeni)
-    button2.place(x=25, y=400)
-
-    button3 = tk.Button(canvas, text="Exit", bg='grey', fg='white', font=("Helvetica", 30, "bold"), width=20, command=exit)
-    button3.place(x=25, y=600)
-
+    button3 = vytvorit_tlacitko("Exit",exit,600)
+    
 main_menu()
 
 
